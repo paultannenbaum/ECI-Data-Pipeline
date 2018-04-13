566079f245034c5bcfc52e5e046842dcd62d9801
Paul Tannenbaum <paultannenbaum@gmail.com>
2018-04-12 17:29:51 -0700

    Add dotenv to project to store environment variables

--- amazon_mws_transform_to_BV5.js
+++ amazon_mws_transform_to_BV5.js
@@ -1,12 +1,17 @@
+// Load Enviroment Variables
+require('dotenv').config()
+
 const fs = require('fs');
 const xml2js = require('xml2js')
 const parser = xml2js.Parser({ explicitArray: false })
 const util = require('util')
 
-const sampleFile = 'sample_amazon_orders.xml'
+console.log(process.env.MWS_DEV_ID)
 
-fs.readFile(sampleFile, (err, data) => {
-  parser.parseString(data, (err, result) => {
-    console.log(util.inspect(result, false, null))
-  })
-})
+// const sampleFile = 'sample_amazon_orders.xml'
+//
+// fs.readFile(sampleFile, (err, data) => {
+//   parser.parseString(data, (err, result) => {
+//     console.log(util.inspect(result, false, null))
+//   })
+// })
