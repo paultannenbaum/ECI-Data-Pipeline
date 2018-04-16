2e7ec149d370397e1684f3e5641102d01a24f396
Paul Tannenbaum <paultannenbaum@gmail.com>
2018-04-16 10:47:26 -0700

    Data pulling

--- amazon_mws_transform_to_BV5.js
+++ amazon_mws_transform_to_BV5.js
@@ -9,37 +9,52 @@ const MWS_MARKETPLACE_ID = process.env.MWS_MARKETPLACE_ID
 
 // Packages
 const fs = require('fs');
-const xml2js = require('xml2js')
-const parser = xml2js.Parser({ explicitArray: false })
 const util = require('util')
 const amazonMws = require('amazon-mws')(MWS_ACCESS_KEY_ID, MWS_SECRET_KEY)
 const moment = require('moment')
 
+// Variables
 const today = moment().subtract(25, 'day').seconds(0).milliseconds(0).toISOString()
 const yesterday = moment().subtract(30, 'day').seconds(0).milliseconds(0).toISOString()
 
-console.log(yesterday, today)
+// Functions
+const logError = (error) => console.log('error:', error);
 
-amazonMws.orders.search({
+const logResponse = (response) => console.log('response', util.inspect(response, false, null));
+
+const fetchYesterdaysOrders = () => amazonMws.orders.search({
     'Version': '2013-09-01',
     'Action': 'ListOrders',
     'SellerId': MWS_SELLER_ID,
     'MarketplaceId.Id.1': MWS_MARKETPLACE_ID,
     'CreatedAfter': yesterday,
     'CreatedBefore': today
-}, function (error, response) {
-    if (error) {
-        console.log('error ', error);
-        return;
-    }
-    console.log('response', util.inspect(response, false, null));
-    console.log(response.Orders.Order.length);
-});
+})
 
-// const sampleFile = 'sample_amazon_orders.xml'
+const fetchOrderItems = (orderId) => amazonMws.orders.search({
+    'Version': '2013-09-01',
+    'Action': 'ListOrderItems',
+    'SellerId': MWS_SELLER_ID,
+    'MarketplaceId.Id.1': MWS_MARKETPLACE_ID,
+    'AmazonOrderId': orderId
+})
 
-// fs.readFile(sampleFile, (err, data) => {
-//   parser.parseString(data, (err, result) => {
-//     console.log(util.inspect(result, false, null))
-//   })
-// })
+
+const testem = () => fetchYesterdaysOrders().then((response) => {
+  console.log(response.Orders.Order.length)
+
+  const orders = response.Orders.Order
+
+  console.log(orders)
+
+  orders.forEach((order) => fetchOrderItems(order.AmazonOrderId).then((result) => {
+      console.log(util.inspect(result, false, null))
+  }))
+}).catch((e) => logResponse(e));
+
+testem()
+
+
+// TODO: Error handling
+// TODO: Throttling
+// TODO: Handle non arrays
