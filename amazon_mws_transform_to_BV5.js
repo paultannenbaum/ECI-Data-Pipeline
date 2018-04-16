5e14bf2f4cfa7d24ac5b41ba2d34c6765a2addc7
Paul Tannenbaum <paultannenbaum@gmail.com>
2018-04-16 16:43:47 -0700

    Basic template in place

--- amazon_mws_transform_to_BV5.js
+++ amazon_mws_transform_to_BV5.js
@@ -12,17 +12,18 @@ const fs = require('fs');
 const util = require('util')
 const amazonMws = require('amazon-mws')(MWS_ACCESS_KEY_ID, MWS_SECRET_KEY)
 const moment = require('moment')
+const builder = require('xmlbuilder');
 
-// Variables
+// Logic
 const today = moment().subtract(25, 'day').seconds(0).milliseconds(0).toISOString()
+
 const yesterday = moment().subtract(30, 'day').seconds(0).milliseconds(0).toISOString()
 
-// Functions
 const logError = (error) => console.log('error:', error);
 
 const logResponse = (response) => console.log('response', util.inspect(response, false, null));
 
-const fetchYesterdaysOrders = () => amazonMws.orders.search({
+const fetchOrders = () => amazonMws.orders.search({
     'Version': '2013-09-01',
     'Action': 'ListOrders',
     'SellerId': MWS_SELLER_ID,
@@ -39,21 +40,112 @@ const fetchOrderItems = (orderId) => amazonMws.orders.search({
     'AmazonOrderId': orderId
 })
 
+const buildXML = (orders) => {
+  const template = {
+    cXML: {
+      '@xmllang': 'en_US',
+      '@version': '1.0',
+      Header: {
+        From: null,
+        To: null,
+        Sender: {
+          UserAgent: 'default'
+        }
+      },
+      Request: {
+        OrderRequest: {
+          OrderRequestHeader: {
+            '@agreementID': '',
+            '@type': 'new',
+            '@orderID': 1234,
+            Total: null,
+            ShipTo: {
+              Address: {
+                '@addressID': 01,
+                '@isoCountryCode': 'US',
+                Name: {
+                  '@xmllang': 'en',
+                  '#text': 'var - Address line 1'
+                },
+                PostalAddress: {
+                  '@name': 'default',
+                  DeliverTo: 'var - Address line 2',
+                  Street: 'var - Address line 3',
+                  Street: 'var - Address line 4',
+                  City: 'var - City',
+                  State: 'CA',
+                  PostalCode: 'Postal Code',
+                  Country: {
+                    '@isoCountryCode': 'US'
+                  }
+                }
+              }
+            },
+            Payment: null,
+            Contact: {
+              Name: {
+                '@xmllang': 'en',
+                '#text': 'Amazon Buyers Name'
+              }
+            },
+            Comments: 'Phone Number: phone number goes here',
+            Extrinsic: {
+              '@name': 'Issuing Office',
+              '#text': 'Amazon'
+            },
+            Extrinsic: {
+              '@name': 'Requisition Office',
+              '#text': 'Sales Order'
+            },
+            Extrinsic: {
+              '@name': 'Accounting and Appropriation'
+            },
+            Extrinsic: {
+              '@name': 'Note'
+            }
+          },
+          ItemOut: {
+            '@lineNumber': 1,
+            '@quantity': 1,
+            ItemID: {
+              SupplierPartID: 'HEWCF340A'
+            },
+            ItemDetail: {
+              UnitPrice: {
+                Money: {
+                  '@currency': 'USD',
+                  '#text': 'line item sell price'
+                }
+              },
+              Description: {
+                '@xmllang': 'en'
+              },
+              UnitOfMeasure: null,
+              ManufacturerPartID: null,
+              ManufacturerName
+            }
+          }
+        }
+      }
+    }
+  }
 
-const testem = () => fetchYesterdaysOrders().then((response) => {
-  console.log(response.Orders.Order.length)
+  const feed = builder.create(template, { encoding: 'utf-8' })
+  //console.log(feed.end({ pretty: true }))
+}
 
-  const orders = response.Orders.Order
+const init = () => fetchOrders().then((ordersData) => {
+  const _orders = ordersData.Orders.Order
+  let orders = [] // Amazon doesn't give us detailed order info, so we must make individual requests for each order
 
-  console.log(orders)
+  _orders.forEach((order) => fetchOrderItems(order.AmazonOrderId).then((orderData) => {
+      orders.push(orderData)
+      // Once all orders have been recieved, go ahead and create XML
+      if (orders.length === _orders.length) { buildXML(orders) }
+  })).catch((e) => logResponse(e))
+}).catch((e) => logResponse(e))
 
-  orders.forEach((order) => fetchOrderItems(order.AmazonOrderId).then((result) => {
-      console.log(util.inspect(result, false, null))
-  }))
-}).catch((e) => logResponse(e));
-
-testem()
-
+buildXML()
 
 // TODO: Error handling
 // TODO: Throttling
